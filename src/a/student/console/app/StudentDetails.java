5632eebf7b68a1af919d02bd5efaf08064b6abc8
universalcodes <yogeshkamra25@gmail.com>
2026-04-23 07:46:06 +0530

    Day 1 - Student Management Project Started Basic Set up is done

---
+++ src/a/student/console/app/StudentDetails.java
@@ -0,0 +1,9 @@
+package a.student.console.app;
+
+public class StudentDetails {
+    public static void main(String[] args) {
+        System.out.println("**********------Student Management System----------***********************");
+
+
+    }
+}
