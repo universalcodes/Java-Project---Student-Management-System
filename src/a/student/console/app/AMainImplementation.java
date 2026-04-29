c02e5a69871eea8af1449e8dd34218c44fcd933e
universalcodes <yogeshkamra25@gmail.com>
2026-04-29 12:38:50 +0530

    Day 3 - Completed Basic Snippnet of the Course.

--- src/a/student/console/app/AMainImplementation.java
+++ src/a/student/console/app/AMainImplementation.java
@@ -10,28 +10,43 @@ public class AMainImplementation {
 
 
         //Usecase 1 Data
+        System.out.println("Printing Student Details 1");
+
+
         BStudentDetails printFirstStudentDetails1 = new BStudentDetails("JAVA USER", 16, "A-34567");
+        printFirstStudentDetails1.enrollNewCourse("Java");
+        printFirstStudentDetails1.enrollNewCourse("QA Lead");
+        printFirstStudentDetails1.enrollNewCourse("Phyton");
+        System.out.println(printFirstStudentDetails1); // printing the student details through methods
+        printFirstStudentDetails1.retrieveStudentInfo(); // printing the method based on toString method
+
+        System.out.println("Printing Student Details 2");
+
+
         //Usecase 2 Data
         BStudentDetails printFirstStudentDetails2 = new BStudentDetails("JaVa UsER", 16, "T-34562");
-        //Usecase 3 Data
-        BStudentDetails printFirstStudentDetails3 = new BStudentDetails("java User", 16, "v-35451");
-        //Usecase 4 Data
-        BStudentDetails printFirstStudentDetails4 = new BStudentDetails("Java", 16, "D-34562");
-        //Usecase 5 Data
-        BStudentDetails printFirstStudentDetails5 = new BStudentDetails("java Important User", 16, "f-23456");
-        //Usecase 6 Data
-        BStudentDetails printFirstStudentDetails6 = new BStudentDetails("java Important User", 16, "f-234561A");
-        //Usecase 6 Data
-        BStudentDetails printFirstStudentDetails7 = new BStudentDetails("java Important User", 16, "%3456");
-
-
-        System.out.println(printFirstStudentDetails1); // printing the student details through methods
+        printFirstStudentDetails2.enrollNewCourse("Playwright");
+        printFirstStudentDetails2.enrollNewCourse("Javascript");
         System.out.println(printFirstStudentDetails2); // printing the student details through methods
-        System.out.println(printFirstStudentDetails3); // printing the student details through methods
-        System.out.println(printFirstStudentDetails4); // printing the student details through methods
-        System.out.println(printFirstStudentDetails5); // printing the student details through methods
-        System.out.println(printFirstStudentDetails6); // printing the student details through methods
-        System.out.println(printFirstStudentDetails7); // printing the student details through methods
+        printFirstStudentDetails2.retrieveStudentInfo(); // printing the method based on toString method
+
+//        //Usecase 3 Data
+//        BStudentDetails printFirstStudentDetails3 = new BStudentDetails("java User", 16, "v-35451");
+//        //Usecase 4 Data
+//        BStudentDetails printFirstStudentDetails4 = new BStudentDetails("Java", 16, "D-34562");
+//        //Usecase 5 Data
+//        BStudentDetails printFirstStudentDetails5 = new BStudentDetails("java Important User", 16, "f-23456");
+//        //Usecase 6 Data
+//        BStudentDetails printFirstStudentDetails6 = new BStudentDetails("java Important User", 16, "f-234561A");
+//        //Usecase 6 Data
+//        BStudentDetails printFirstStudentDetails7 = new BStudentDetails("java Important User", 16, "%3456");
+
+//
+//        System.out.println(printFirstStudentDetails3); // printing the student details through methods
+//        System.out.println(printFirstStudentDetails4); // printing the student details through methods
+//        System.out.println(printFirstStudentDetails5); // printing the student details through methods
+//        System.out.println(printFirstStudentDetails6); // printing the student details through methods
+//        System.out.println(printFirstStudentDetails7); // printing the student details through methods
         //printFirstStudentDetails.retraiveStudentInfo(); // printing the method based on toString method
     }
 }
--- src/a/student/console/app/BStudentDetails.java
+++ src/a/student/console/app/BStudentDetails.java
@@ -13,9 +13,8 @@ public class BStudentDetails {
     private String studentID;
     private List<String> coursesList;
 
-    BStudentDetails(String name, int age, String studentID)
-    {
-        if(validateStudentAge(age) && validateStudentName(name) && validateStudentID(studentID))  {
+    BStudentDetails(String name, int age, String studentID) {
+        if (validateStudentAge(age) && validateStudentName(name) && validateStudentID(studentID)) {
             this.name = name;
             this.age = age;
             this.studentID = studentID;
@@ -23,42 +22,39 @@ public class BStudentDetails {
         }
     }
 
-    public void enrollNewCourse(String newCourseEnrolled)
-    {
-        coursesList.add(newCourseEnrolled);
+    public void enrollNewCourse(String newCourseEnrolled) {
+        if (!coursesList.contains(newCourseEnrolled) && validateCourseList(newCourseEnrolled)) {
+            coursesList.add(newCourseEnrolled);
+            System.out.println("New Course named as " + newCourseEnrolled + " has been added successfully ");
+        } else {
+            System.out.println("Invalid Course : "+ newCourseEnrolled);
+        }
     }
 
-    public void retraiveStudentInfo()
-    {
-        System.out.println("Student Name as Follows\t"+name);
-        System.out.println("Student Age as Follows\t"+age);
-        System.out.println("Student ID as Follows\t"+studentID);
-        System.out.println("Student Courses as Follows\t"+coursesList);
+    public void retrieveStudentInfo() {
+        System.out.println("Student Name as Follows\t" + name);
+        System.out.println("Student Age as Follows\t" + age);
+        System.out.println("Student ID as Follows\t" + studentID);
+        System.out.println("Student Courses as Follows\t" + coursesList);
     }
 
     @Override
-    public String toString()
-    {
-        return "BStudentDetails [ name = "+ name + "]" + "["+ "age = "+ age + "]" + "["+ "studentID = "+ studentID +  "]" + "["+ "Courses = "+ coursesList +  "]" ;
+    public String toString() {
+        return "BStudentDetails [ name = " + name + "]" + "[" + "age = " + age + "]" + "[" + "studentID = " + studentID + "]" + "[" + "Courses = " + coursesList + "]";
     }
 
 
-    private boolean validateStudentAge(int age)
-    {
-        if(age >=14 && age<=70)
-        {
+    private boolean validateStudentAge(int age) {
+        if (age >= 14 && age <= 70) {
             return true;
-        }
-        else
-        {
+        } else {
             System.err.println("Student age must be between 14 & 70");
         }
         return false;
     }
 
 
-    private boolean validateStudentName(String name)
-    {
+    private boolean validateStudentName(String name) {
         // In the following code where we have used regular expression , where A-Z Alphabets are allowed with following formats : 1) Uppercase 2) Lowercase
         // \\s is the regular expression which helps to allowed spaces within the names
         // +$ which helps String must be ended with Alphabet
@@ -67,41 +63,54 @@ public class BStudentDetails {
         Pattern compileTheGivenPattern = Pattern.compile(namePattern);
         Matcher matchPatternResult = compileTheGivenPattern.matcher(name);
 
-        if(matchPatternResult.matches())
-        {
+        if (matchPatternResult.matches()) {
             return true;
-        }
-        else
-        {
+        } else {
             System.err.println("Name is Invalid, Only Alphabets are allowed");
         }
         return false;
     }
 
-    private boolean validateStudentID(String studentID)
-    {
+    private boolean validateStudentID(String studentID) {
         String idPattern = "^[a-zA-Z]-\\d+$";
         Pattern compilePatternNumber = Pattern.compile(idPattern);
         Matcher matchPatternNumberResult = compilePatternNumber.matcher(studentID);
 
-        if (matchPatternNumberResult.matches())
-        {
+        if (matchPatternNumberResult.matches()) {
             return true;
-        }
-        else
-        {
+        } else {
             System.out.println("Invalid Student ID, ");
             return false;
         }
 
     }
 
-    public static void main(String[] args) {
-        System.out.println("Step 1 - Declare Instance (Non Static )Variable within the class");
-        System.out.println("Step 2 - Create Constructor & Define All Instance (Non Static ) Variable using this keyword ");
-        System.out.println("Step 3 - create One Method  where it prints all Values");
-        System.out.println("Step 4 - Optional if we want to print all Variables in Single line use ToString() method");
+    public boolean validateCourseList(String enrolledCourseName) {
+        if (enrolledCourseName.equalsIgnoreCase("Manual Testing") || enrolledCourseName.equalsIgnoreCase("Java") ||
+                enrolledCourseName.equalsIgnoreCase("Javascript") || enrolledCourseName.equalsIgnoreCase("TypeScript") ||
+                enrolledCourseName.equalsIgnoreCase("Selenium") || enrolledCourseName.equalsIgnoreCase("Playwright") ||
+                enrolledCourseName.equalsIgnoreCase("SQL") || enrolledCourseName.equalsIgnoreCase("Prosgres") ||
+                enrolledCourseName.equalsIgnoreCase("GIT") || enrolledCourseName.equalsIgnoreCase("CI/CD Pipeline") ||
+                enrolledCourseName.equalsIgnoreCase("Docker Deployment") || enrolledCourseName.equalsIgnoreCase("AWS Testing") ||
+                enrolledCourseName.equalsIgnoreCase("Jmeter") || enrolledCourseName.equalsIgnoreCase("SDET") ||
+                enrolledCourseName.equalsIgnoreCase("QA Lead") || enrolledCourseName.equalsIgnoreCase("SDET Essentials")) {
+            System.out.println("Valid Course is Selected from list :"+enrolledCourseName);
+                return true;
 
+        }
 
+        else
+        {
+            System.out.println(enrolledCourseName + " is not part of Course provided in the list");
+            return false;
+        }
     }
-}
+        public static void main (String[] args){
+            System.out.println("Step 1 - Declare Instance (Non Static )Variable within the class");
+            System.out.println("Step 2 - Create Constructor & Define All Instance (Non Static ) Variable using this keyword ");
+            System.out.println("Step 3 - create One Method  where it prints all Values");
+            System.out.println("Step 4 - Optional if we want to print all Variables in Single line use ToString() method");
+
+
+        }
+    }
